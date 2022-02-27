88d2ab37645819f970e4bc581212e9aa69e79892
myrasaet <74717647+myrasaet@users.noreply.github.com>
2022-02-27 21:01:35 +0900

    improve codes

--- src/main.py
+++ src/main.py
@@ -2,14 +2,13 @@ from summarizer.summarize import generate_summary
 from action_item_classifier.pred import pred_action_items
 
 filename = input("Enter filename here (path/to/text/file.txt): ")
-# input_text = ""
 f = open(filename, 'r')
 input_text = f.read()
 
 summary = generate_summary(input_text)
 action_items = pred_action_items(summary)
 print('SUMMARY:', summary)
-if len(action_items) == 0:
+if not action_items:
     print("No action items!")
 else:
     print('ACTION ITEMS:', action_items)
